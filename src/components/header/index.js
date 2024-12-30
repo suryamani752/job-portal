0eb0263eeecfd4841c1deae418288a1fca757204
Suryamani kumar <124435021+suryamani752@users.noreply.github.com>
2024-12-30 11:46:59 +0530

    fix profile css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -61,37 +61,46 @@ function Header({ user, profileInfo }) {
   return (
     <div>
       <header className="flex h-16 w-full shrink-0 items-center">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button className="lg:hidden">
-              <AlignJustify className="h-6 w-6" />
-              <span className="sr-only">Toggle Navigation Menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <Link className="mr-6 hidden lg:flex" href={"#"}>
-              <h3>JOB PORTAL</h3>
-            </Link>
-            <div className="grid gap-2 py-6">
-              {menuItems.map((menuItem) =>
-                menuItem.show ? (
-                  <Link
-                    href={menuItem.path}
-                    className="flex w-full items-center py-2 text-lg font-semibold"
-                  >
-                    {menuItem.label}
-                  </Link>
-                ) : null
-              )}
-              <Moon
-                className="cursor-pointer mb-4"
-                fill={theme === "dark" ? "light" : "dark"}
-                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-              />
-              <UserButton afterSignOutUrl="/" />
-            </div>
-          </SheetContent>
-        </Sheet>
+        <div className="flex justify-center items-center gap-9">
+          <div>
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button className="lg:hidden">
+                  <AlignJustify className="h-6 w-6" />
+                  <span className="sr-only">Toggle Navigation Menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left">
+                <Link className="mr-6 hidden lg:flex" href={"#"}>
+                  <h3>JOB PORTAL</h3>
+                </Link>
+                <div className="grid gap-2 py-6">
+                  {menuItems.map((menuItem) =>
+                    menuItem.show ? (
+                      <Link
+                        href={menuItem.path}
+                        className="flex w-full items-center py-2 text-lg font-semibold"
+                      >
+                        {menuItem.label}
+                      </Link>
+                    ) : null
+                  )}
+                  <Moon
+                    className="cursor-pointer mb-4"
+                    fill={theme === "dark" ? "light" : "dark"}
+                    onClick={() =>
+                      setTheme(theme === "light" ? "dark" : "light")
+                    }
+                  />
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+          <div>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+
         <Link className="hidden font-bold text-3xl lg:flex mr-6" href={"/"}>
           JOB PORTAL
         </Link>
